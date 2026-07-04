95ca409b2789d86b1bf955da697e32dc33d9a167
stepann <stepan.snegovskoi@mail.ru>
2026-07-04 19:12:42 +0300

    chore: move WorthItApp and MainActivity to other packages

---
+++ app/src/main/java/com/metes/worthit/app/WorthItApp.kt
@@ -0,0 +1,8 @@
+package com.metes.worthit.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class WorthItApp : Application() {
+}
---
+++ app/src/main/java/com/metes/worthit/ui/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.metes.worthit.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.metes.worthit.ui.navigation.AppNavigation
+import com.metes.worthit.ui.screen.main.ItemsRoute
+import com.metes.worthit.ui.theme.WorthItTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            WorthItTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    val navController = rememberNavController()
+
+                    AppNavigation(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        navHostController = navController
+                    )
+                }
+            }
+        }
+    }
+}
