93938523f3427ca28807d5c56167af148e6e5169
stepann <stepan.snegovskoi@mail.ru>
2026-07-04 20:21:47 +0300

    build: add coil

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,8 @@ dependencies {
 
     implementation(libs.navigation.compose)
 
+    implementation(libs.coil.compose)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
