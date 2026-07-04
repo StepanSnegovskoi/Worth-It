60543f93bc74fe1e6bf18fea0accee38ef29eb19
stepann <stepan.snegovskoi@mail.ru>
2026-07-04 19:21:09 +0300

    feat(domain): add InsertItem and ObserveItems use cases

---
+++ app/src/main/java/com/metes/worthit/domain/usecase/InsertItemUseCase.kt
@@ -0,0 +1,16 @@
+package com.metes.worthit.domain.usecase
+
+import com.metes.worthit.domain.entity.Item
+import com.metes.worthit.domain.repository.ItemsRepository
+import javax.inject.Inject
+
+class InsertItemUseCase @Inject constructor(
+    private val repository: ItemsRepository
+) {
+    suspend operator fun invoke(
+        name: String,
+    ) {
+        val item = Item(name = name)
+        repository.insertItem(item)
+    }
+}
---
+++ app/src/main/java/com/metes/worthit/domain/usecase/ObserveItemsUseCase.kt
@@ -0,0 +1,14 @@
+package com.metes.worthit.domain.usecase
+
+import com.metes.worthit.domain.entity.Item
+import com.metes.worthit.domain.repository.ItemsRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class ObserveItemsUseCase @Inject constructor(
+    private val repository: ItemsRepository
+) {
+    operator fun invoke(): Flow<List<Item>> {
+        return repository.observeItems()
+    }
+}
