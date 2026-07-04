9a485ab2c84a7bfd9840358162dd5aaa79bf11f8
stepann <stepan.snegovskoi@mail.ru>
2026-07-04 19:09:40 +0300

    feat: add mappers for Item

--- app/src/main/java/com/metes/worthit/data/local/entity/ItemDbModel.kt
+++ app/src/main/java/com/metes/worthit/data/local/entity/ItemDbModel.kt
@@ -2,6 +2,7 @@ package com.metes.worthit.data.local.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.metes.worthit.domain.entity.Item
 
 @Entity(tableName = "items")
 data class ItemDbModel(
@@ -9,3 +10,15 @@ data class ItemDbModel(
     val id: Int,
     val name: String,
 )
+
+fun Item.toDbModel() = ItemDbModel(
+    id = id,
+    name = name
+)
+
+fun ItemDbModel.toEntity() = Item(
+    id = id,
+    name = name
+)
+
+fun List<ItemDbModel>.toEntities() = map { it.toEntity() }
--- app/src/main/java/com/metes/worthit/domain/entity/Item.kt
+++ app/src/main/java/com/metes/worthit/domain/entity/Item.kt
@@ -1,6 +1,10 @@
 package com.metes.worthit.domain.entity
 
 data class Item(
-    val id: Int,
+    val id: Int = DEFAULT_ID,
     val name: String,
-)
+) {
+    companion object {
+        private const val DEFAULT_ID = 0
+    }
+}
