419cb6e3faaec0f96d36904a7949e3c0ea0265e0
stepann <stepan.snegovskoi@mail.ru>
2026-07-06 16:10:44 +0300

    feat(items): init Items screen and viewmodel

--- app/src/main/java/com/metes/worthit/ui/screen/main/ItemsScreen.kt
+++ app/src/main/java/com/metes/worthit/ui/screen/main/ItemsScreen.kt
@@ -1,21 +1,71 @@
 package com.metes.worthit.ui.screen.main
 
-import androidx.compose.material3.Text
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.metes.worthit.R
+import com.metes.worthit.domain.entity.Item
+import com.metes.worthit.ui.screen.main.component.Item
+import com.metes.worthit.ui.screen.main.component.Items
 
 @Composable
 fun ItemsRoute(
     modifier: Modifier = Modifier,
-    viewModel: ItemsViewModel = hiltViewModel()
+    viewModel: ItemsViewModel = hiltViewModel(),
+    onNavigateToAddItem: () -> Unit
 ) {
-    ItemsScreen(modifier = modifier)
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    LaunchedEffect(Unit) {
+        viewModel.events.collect { event ->
+            when (event) {
+                ItemsEvent.NavigateToAddItem -> onNavigateToAddItem()
+            }
+        }
+    }
+
+    ItemsScreen(
+        items = uiState.items,
+        modifier = modifier,
+        onAddItemClick = { viewModel.processCommand(ItemsCommand.AddItem) },
+    )
 }
 
 @Composable
 fun ItemsScreen(
-    modifier: Modifier = Modifier
+    items: List<Item>,
+    modifier: Modifier = Modifier,
+    onAddItemClick: () -> Unit
 ) {
-    Text(text = "ItemsScreen")
+    Scaffold(
+        modifier = modifier,
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onAddItemClick
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.add_24dp),
+                    contentDescription = stringResource(R.string.add_item_desc)
+                )
+            }
+        }
+    ) { innerPadding ->
+        Items(
+            items = items,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        )
+    }
 }
--- app/src/main/java/com/metes/worthit/ui/screen/main/ItemsViewModel.kt
+++ app/src/main/java/com/metes/worthit/ui/screen/main/ItemsViewModel.kt
@@ -1,6 +1,66 @@
 package com.metes.worthit.ui.screen.main
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.metes.worthit.domain.entity.Item
+import com.metes.worthit.domain.usecase.InsertItemUseCase
+import com.metes.worthit.domain.usecase.ObserveItemsUseCase
+import com.metes.worthit.ui.screen.add_item.AddItemState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.SendChannel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class ItemsViewModel : ViewModel() {
+@HiltViewModel
+class ItemsViewModel @Inject constructor(
+    private val observeItemsUseCase: ObserveItemsUseCase,
+    private val savedStateHandle: SavedStateHandle
+) : ViewModel() {
+
+    val uiState = observeItemsUseCase().map { items ->
+        ItemsState(items = items)
+    }.stateIn(
+        scope = viewModelScope,
+        initialValue = ItemsState(),
+        started = SharingStarted.WhileSubscribed(stopTimeoutMillis = 5000)
+    )
+
+    private val _events = Channel<ItemsEvent>()
+    val events = _events.receiveAsFlow()
+
+    fun processCommand(command: ItemsCommand) {
+        when (command) {
+            ItemsCommand.AddItem -> {
+                viewModelScope.launch {
+                    _events.send(ItemsEvent.NavigateToAddItem)
+                }
+            }
+        }
+    }
+}
+
+sealed interface ItemsCommand {
+    data object AddItem : ItemsCommand
+}
+
+sealed interface ItemsEvent {
+    data object NavigateToAddItem : ItemsEvent
+}
+
+data class ItemsState(
+    val items: List<Item> = INITIAL_ITEMS
+) {
+    companion object {
+        private val INITIAL_ITEMS = emptyList<Item>()
+    }
 }
