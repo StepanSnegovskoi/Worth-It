ad9355be6c68fdee8a3b412fa9ce2e5685d9a144
stepann <stepan.snegovskoi@mail.ru>
2026-07-09 14:13:57 +0300

    fix(navigation): remove animations

--- app/src/main/java/com/metes/worthit/ui/navigation/AppNavigation.kt
+++ app/src/main/java/com/metes/worthit/ui/navigation/AppNavigation.kt
@@ -1,11 +1,22 @@
 package com.metes.worthit.ui.navigation
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.core.content.IntentCompat
+import androidx.core.net.UriCompat
+import androidx.core.os.BundleCompat
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navDeepLink
 import com.metes.worthit.ui.screen.add_item.AddItemRoute
 import com.metes.worthit.ui.screen.main.ItemsRoute
 
@@ -17,7 +28,13 @@ fun AppNavigation(
     NavHost(
         navController = navHostController,
         startDestination = Screen.Items,
-        modifier = modifier
+        modifier = modifier,
+        enterTransition = {
+            EnterTransition.None
+        },
+        exitTransition = {
+            ExitTransition.None
+        }
     ) {
         composable<Screen.Items> {
             ItemsRoute(
