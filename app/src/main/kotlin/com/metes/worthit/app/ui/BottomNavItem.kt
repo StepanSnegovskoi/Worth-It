a2d8d36edaab74be18119f1e83179c3d1dcc90a7
stepann <stepan.snegovskoi@mail.ru>
2026-08-20 01:42:44 +0300

    fix(app): remove useless BottomNavItem routeClass property

--- app/src/main/kotlin/com/metes/worthit/app/ui/BottomNavItem.kt
+++ app/src/main/kotlin/com/metes/worthit/app/ui/BottomNavItem.kt
@@ -9,7 +9,6 @@ import kotlin.reflect.KClass
 
 data class BottomNavItem(
     val route: Screen,
-    val routeClass: KClass<out Any>,
     @param:StringRes val titleResId: Int,
     @param:DrawableRes val iconResId: Int,
 )
@@ -17,19 +16,16 @@ data class BottomNavItem(
 val bottomNavItems = listOf(
     BottomNavItem(
         route = Screen.Items,
-        routeClass = Screen.Items::class,
         titleResId = R.string.items_screen,
         iconResId = DesignR.drawable.items_24dp,
     ),
     BottomNavItem(
         route = Screen.SaveItem(),
-        routeClass = Screen.SaveItem::class,
         titleResId = R.string.save_item_screen,
         iconResId = DesignR.drawable.edit_24dp,
     ),
     BottomNavItem(
         route = Screen.Settings,
-        routeClass = Screen.Settings::class,
         titleResId = R.string.settings_screen,
         iconResId = DesignR.drawable.settings_24dp,
     ),
