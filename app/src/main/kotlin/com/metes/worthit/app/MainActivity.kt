8c1f7e4508a47525af74ad35d3f2f0ddda8826c8
stepann <stepan.snegovskoi@mail.ru>
2026-08-20 20:40:12 +0300

    style(app): disable navigation bar contrast enforced

--- app/src/main/kotlin/com/metes/worthit/app/MainActivity.kt
+++ app/src/main/kotlin/com/metes/worthit/app/MainActivity.kt
@@ -2,6 +2,7 @@ package com.metes.worthit.app
 
 import android.content.Intent
 import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -37,6 +38,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import kotlinx.serialization.serializer
 import javax.inject.Inject
+import androidx.core.graphics.toColorInt
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -65,7 +67,15 @@ class MainActivity : ComponentActivity() {
                             lightScrim = Color.TRANSPARENT,
                             darkScrim = Color.TRANSPARENT,
                         ) { isDarkTheme },
+                        navigationBarStyle = SystemBarStyle.auto(
+                            lightScrim = Color.TRANSPARENT,
+                            darkScrim = Color.TRANSPARENT
+                        ) { isDarkTheme }
                     )
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                        window.isNavigationBarContrastEnforced = false
+                    }
                 }
             }
         }
