5b40e70b2f88d2906086746cde022622f4fa8295
stepann <stepan.snegovskoi@mail.ru>
2026-08-21 19:49:48 +0300

    build(build-logic): add task for counting file lines

--- build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
@@ -1,5 +1,6 @@
 
 import com.android.build.api.dsl.ApplicationExtension
+import com.metes.worthit.convention.CountCodeLinesTask
 import com.metes.worthit.convention.configureKotlinAndroid
 import com.metes.worthit.convention.libs
 import org.gradle.api.Plugin
@@ -7,6 +8,7 @@ import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.register
 
 class AndroidApplicationConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -15,6 +17,19 @@ class AndroidApplicationConventionPlugin : Plugin<Project> {
 
             extensions.configure<ApplicationExtension> {
                 configureKotlinAndroid(this)
+            }
+
+            tasks.register<CountCodeLinesTask>("countCodeLinesTask") {
+                sourceFiles.from(rootProject.layout.projectDirectory)
+
+                sourceFiles.include("**/*.kt")
+                sourceFiles.exclude("**/build/**")
+                sourceFiles.exclude("**/androidTest/**")
+                sourceFiles.exclude("**/test/**")
+            }
+
+            tasks.named("check") {
+                dependsOn("countCodeLinesTask")
             }
         }
     }
---
+++ build-logic/convention/src/main/kotlin/com/metes/worthit/convention/CountCodeLinesTask.kt
@@ -0,0 +1,83 @@
+package com.metes.worthit.convention
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.file.ConfigurableFileTree
+import org.gradle.api.file.RegularFileProperty
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.CacheableTask
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.InputFiles
+import org.gradle.api.tasks.OutputFile
+import org.gradle.api.tasks.PathSensitive
+import org.gradle.api.tasks.PathSensitivity
+import org.gradle.api.tasks.TaskAction
+import java.io.File
+
+enum class SortType {
+    ASCENDING_ROWS,
+    DESCENDING_ROWS;
+}
+
+@CacheableTask
+abstract class CountCodeLinesTask : DefaultTask() {
+
+    @get:Input
+    abstract val sortType: Property<SortType>
+
+    @get:InputFiles
+    @get:PathSensitive(PathSensitivity.RELATIVE)
+    abstract val sourceFiles: ConfigurableFileTree
+
+    @get:OutputFile
+    abstract val outputFile: RegularFileProperty
+
+    init {
+        outputFile.convention(project.layout.buildDirectory.file("code_lines.md"))
+        sortType.convention(SortType.DESCENDING_ROWS)
+    }
+
+    @TaskAction
+    fun countCodeLines() {
+        val outputFile = outputFile.get().asFile
+
+        writeHeader(outputFile)
+        writeHeaderOfTable(outputFile)
+        writeFilesInfoInTable(outputFile)
+    }
+
+    private fun writeHeader(file: File) {
+        file.writeText("""
+            # Count of Lines
+        """.trimIndent().plus("\n"))
+    }
+
+    private fun writeHeaderOfTable(file: File) {
+        file.appendText("""
+            |№|Name|Lines|
+            |-|----|-----|
+        """.trimIndent().plus("\n"))
+    }
+
+    private fun writeFilesInfoInTable(file: File) {
+        val sortType = sortType.get()
+
+        val sortedFiles = sourceFiles.sortedBy {
+            if (sortType == SortType.DESCENDING_ROWS) {
+                -it.readLines().count()
+            } else {
+                it.readLines().count()
+            }
+        }
+
+        var i = 1
+        var total = 0
+        val content = sortedFiles.joinToString("\n") { file ->
+            val codeLines = file.readLines().count()
+            total += codeLines
+            "|${i++}|${file.name}|$codeLines|"
+        }
+
+        file.appendText("|Total|Total|$total|\n")
+        file.appendText(content)
+    }
+}
