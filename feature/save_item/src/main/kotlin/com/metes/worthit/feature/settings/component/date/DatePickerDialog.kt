7ed175e77cfb11cb6416e2742e278ac4c92a48d1
stepann <stepan.snegovskoi@mail.ru>
2026-08-25 15:27:52 +0300

    fix(save_item): fix material3 DatePicker content color for inner icon for year navigation

--- feature/save_item/src/main/kotlin/com/metes/worthit/feature/settings/component/date/DatePickerDialog.kt
+++ feature/save_item/src/main/kotlin/com/metes/worthit/feature/settings/component/date/DatePickerDialog.kt
@@ -3,12 +3,15 @@ package com.metes.worthit.feature.settings.component.date
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.DatePickerState
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.metes.worthit.core.common.toLocalDateFromUtc
 import com.metes.worthit.core.designsystem.component.defaults.WorthItDatePickerDefaults
 import com.metes.worthit.core.designsystem.component.other.WorthItTextButton
+import com.metes.worthit.core.designsystem.theme.AppTheme
 import com.metes.worthit.feature.save_item.R
 import java.time.LocalDate
 
@@ -36,11 +39,24 @@ internal fun DatePickerDialog(
                 )
             }
         ) {
-            DatePicker(
-                state = state,
-                showModeToggle = false,
-                colors = WorthItDatePickerDefaults.colors(),
-            )
+            /**
+             * bug in material3.
+             *
+             * @see androidx.compose.material3.MonthsNavigation
+             * @see androidx.compose.material3.YearPickerMenuButton
+             *
+             * icon in [androidx.compose.material3.YearPickerMenuButton] doesn't use navigationContentColor
+             * from 'DatePickerColors', so we need to do this below.
+             */
+            CompositionLocalProvider(
+                LocalContentColor provides AppTheme.colorScheme.primary
+            ) {
+                DatePicker(
+                    state = state,
+                    showModeToggle = false,
+                    colors = WorthItDatePickerDefaults.colors(),
+                )
+            }
         }
     }
 }
