5c685437253da8d17aaceff29b03638d97ffb21a
stepann <stepan.snegovskoi@mail.ru>
2026-09-12 14:59:59 +0300

    test(domain): add tests for ResultExt

---
+++ core/domain/src/test/kotlin/com/metes/worthit/core/domain/utils/ResultExtTest.kt
@@ -0,0 +1,61 @@
+package com.metes.worthit.core.domain.utils
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class ResultExtTest {
+
+    private val resultSuccess = Result.Success<Int>(5)
+    private val resultError = Result.Error<Exception>(IllegalArgumentException("Test exception"))
+
+    @Test
+    fun `onSuccess called only when result is Success`() {
+        var isSuccessCalled = false
+
+        resultSuccess.onSuccess {
+            isSuccessCalled = true
+        }
+        resultError.onSuccess {
+            isSuccessCalled = false
+        }
+
+        assertTrue(isSuccessCalled)
+    }
+
+    @Test
+    fun `onError called only when result is Error`() {
+        var isErrorCalled = false
+
+        resultError.onError {
+            isErrorCalled = true
+        }
+        resultSuccess.onError {
+            isErrorCalled = false
+        }
+
+        assertTrue(isErrorCalled)
+    }
+
+    @Test
+    fun `onResult called onSuccess if result is Success`() {
+        var isSuccessInvoked = false
+
+        resultSuccess.onResult(
+            onSuccess = { isSuccessInvoked = true },
+            onError = { isSuccessInvoked = false },
+        )
+        assertEquals(isSuccessInvoked, true)
+    }
+
+    @Test
+    fun `onResult called onError if result is Error`() {
+        var isErrorInvoked = false
+
+        resultError.onResult(
+            onSuccess = { isErrorInvoked = false },
+            onError = { isErrorInvoked = true },
+        )
+        assertEquals(isErrorInvoked, true)
+    }
+}
